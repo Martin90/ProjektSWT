bc81694f6ce457417a7a965f7464b002dc810f4e
Martin90 <martin@kitschmann.de>
2013-07-01 16:14:27 +0200

    Update hallo welt.cpp
    Fertig, kann dann übernommen werden.

--- hallo welt.cpp
+++ hallo welt.cpp
@@ -2,47 +2,17 @@
 
 #include "stdafx.h"
 #include "Form1.h"
-#include "Notify.h"
-
-
-
 
 using namespace hallowelt;
-
-
 
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
-  notify();
+	fileload();//Methode welche die Dateien downloadet.
 	// Aktivieren visueller Effekte von Windows XP, bevor Steuerelemente erstellt werden
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
-
 	// Hauptfenster erstellen und ausführen
 	Application::Run(gcnew Form1());
-	bool check=false;
-	Form1 c;
-	for(;;)
-	{
-		if (check==false){
-			notify();
-			c.test();
-		}
-		check=notify();
-	}
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
